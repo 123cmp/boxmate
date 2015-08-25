c621a281b4891f6280da86049e32e88cc8a0b4bb
MaxKomar <maxkomar65@gmail.com>
2015-08-25 17:05:41 +0300

    add checking of email in base

--- modules/api/index.js
+++ modules/api/index.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var dataBase = require("../dataBase").UsersModel;
+var dataBase = require("../dataBase");
 var bodyParser = require('body-parser');
 var log = require("../myWinston")(module);
 
@@ -25,32 +25,40 @@ module.exports = function(app) {
    });
    app.put("/api/users/", function(req, res) {
 
-      log.info(req.body);
-      log.info(req.body[0].name)
+      dataBase.UsersModel.find({mail: req.body.mail}, function(err, article){
+         if(article.length < 1){
 
-      var user = new dataBase({
-         name: req.body[0].name,
-         mail: req.body[0].mail,
-         pwd: req.body[0].pwd
-      });
-      console.log(user);
+            var user = new dataBase.UsersModel({
+               name: req.body.name,
+               mail: req.body.mail,
+               pwd: req.body.pwd
+            });
 
-      user.save(function (err) {
-         if (!err) {
-            log.info("user created");
-            return res.send({ status: 'OK', user:user});
+            user.save(function (err) {
+               if (!err) {
+                  log.info("user created");
+                  return res.send({ status: 'OK'});
+               } else {
+                  console.log(err);
+                  if(err.name == 'ValidationError') {
+                     res.statusCode = 400;
+                     res.send({ error: 'Validation error' });
+                  } else {
+                     res.statusCode = 500;
+                     res.send({ error: 'Server error' });
+                  }
+                  log.error('Internal error(%d): %s',res.statusCode,err.message);
+               }
+            });
+
          } else {
-            console.log(err);
-            if(err.name == 'ValidationError') {
-               res.statusCode = 400;
-               res.send({ error: 'Validation error' });
-            } else {
-               res.statusCode = 500;
-               res.send({ error: 'Server error' });
-            }
-            log.error('Internal error(%d): %s',res.statusCode,err.message);
+
+            res.statusCode = 400;
+            res.send({ error: 'Email is not available'});
+
          }
       });
+
    });
    app.post("/api/users/", function(req, res) {
 
